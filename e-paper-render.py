b7981db90700e146e2d2bd994bb2138e247c7f83
温海杰 <wenhaijie@light2cloud.com>
2021-05-31 14:10:04 +0800

    修改温度与空气质量间距

--- e-paper-render.py
+++ e-paper-render.py
@@ -361,12 +361,12 @@ try:
         draw_weather.text((105, 160), aqq, font=font24, fill=0)
     elif len(condition.get('temp')) == 2:
         draw_weather.text((60, 120), condition.get('temp'), font=font72, fill=0)
-        today_weather.paste(celsius1, (140, 130))
-        draw_weather.text((140, 160), aqq, font=font24, fill=0)
+        today_weather.paste(celsius1, (150, 130))
+        draw_weather.text((150, 160), aqq, font=font24, fill=0)
     else:
         draw_weather.text((60, 140), condition.get('temp'), font=font72, fill=0)
         today_weather.paste(celsius1, (180, 130))
-        draw_weather.text((180, 180), aqq, font=font24, fill=0)
+        draw_weather.text((180, 200), aqq, font=font24, fill=0)
 
     # 显示室内温度
     sht30.write_command()
